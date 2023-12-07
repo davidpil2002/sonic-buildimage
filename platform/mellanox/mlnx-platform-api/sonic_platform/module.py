0a65f44ef6e48451087529fe914d252db3e29209
David Pilnik <davidpil@nvidia.com>
2023-12-07 12:59:55 +0200

    Add Redis ACL support

--- platform/mellanox/mlnx-platform-api/sonic_platform/module.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/module.py
@@ -43,6 +43,7 @@ class Module(ModuleBase):
     FIELD_SEQ_NO = 'seq_no'
     redis_client = None
 
+
     def __init__(self, slot_id):
         super(Module, self).__init__()
         self.slot_id = slot_id
@@ -130,7 +131,12 @@ class Module(ModuleBase):
         # pull in the redis package (saves ~20MB RSS in psud).
         if cls.redis_client is None:
             import redis
-            cls.redis_client = redis.Redis(db=cls.STATE_DB)
+            username = 'admin'
+            password = utils.read_str_from_file('/etc/shadow_redis_dir/shadow_redis_admin')
+            ca = "/etc/shadow_redis_dir/certs_redis/ca.crt"
+            cls.redis_client = redis.Redis(port=6379, db=cls.STATE_DB, username=username,
+                                           password=password, ssl=True, ssl_cert_reqs=None,
+                                           ssl_ca_certs=ca)
         return cls.redis_client
 
     def _get_seq_no(self):
